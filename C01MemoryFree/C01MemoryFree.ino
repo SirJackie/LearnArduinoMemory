c0d2fa949e5b0ba05232c92afcc10da72bf07771
lyj00912 <3122742760@qq.com>
2024-04-15 14:52:58 +0800

    save progress, noticed sth wrong about my comprehension.

--- C01MemoryFree/C01MemoryFree.ino
+++ C01MemoryFree/C01MemoryFree.ino
@@ -1,10 +1,11 @@
-extern unsigned int __heap_start;  // Initial Heap Top (Compiler's Inexplicit Variable).
-extern void *__brkval;  // Current Heap Top, remains 0 if no malloc() is called.
+extern char *__malloc_heap_start;  // Initial Heap Top (Compiler's Inexplicit Variable).
+// extern void *__brkval;  // Current Heap Top, remains 0 if no malloc() is called.
+extern int __heap_start, *__brkval;
 
 unsigned int GetHeapTop(){
   if ((int)__brkval == 0) {
     // If no malloc() is called, return the Static Top as the Heap Top.
-    return (unsigned int)&__heap_start;
+    return (unsigned int)&__malloc_heap_start;
   } else {
     // If malloc() IS called, return the last malloc() address as the Heap Top.
     return (unsigned int)__brkval;
@@ -12,7 +13,7 @@ unsigned int GetHeapTop(){
 }
 
 unsigned int GetStackBottom(){
-  char top;  // Newly Assigned Local Variable, SHOULD BE at the Stack Bottom.
+  char top = 'F';  // Newly Assigned Local Variable, SHOULD BE at the Stack Bottom.
   return (unsigned int)&top;  // Return Stack Bottom Address.
 }
 
@@ -20,12 +21,38 @@ unsigned int GetMemoryStatus() {
   return GetHeapTop() - GetStackBottom();
 }
 
+int freeRam() {
+  extern int __heap_start,*__brkval;
+  int v;
+  return (int)&v - (__brkval == 0  
+    ? (int)&__heap_start : (int) __brkval);  
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println(GetHeapTop());
+  // Serial.println(GetHeapTop());
+  // Serial.println(GetStackBottom());
+  // Serial.println(GetMemoryStatus());
+
+  // Serial.println((__brkval == 0 ? (int)&__heap_start : (int) __brkval));
+
+
+
+
+  // Serial.println((int)&__heap_start);
+  // Serial.println((int) __brkval);
+
+  // int* dynaArray = new int[256];
+
+  // for (int i = 0; i < 256; i++){
+  //   dynaArray[i] = 127;
+  // }
+
+  // Serial.println((int)&__heap_start);
+  // Serial.println((int) __brkval);
+
   Serial.println(GetStackBottom());
-  Serial.println(GetMemoryStatus());
 }
 
 void loop() {
