ba0eeb64ea16767613c94b13d4c3ee6c7ba3264e
lyj00912 <3122742760@qq.com>
2024-04-15 15:43:44 +0800

    BUG SOLVED! Found a way to stop compiler optimization (subtraction operation with an extern int).

--- C02MemoryWorking/C02MemoryWorking.ino
+++ C02MemoryWorking/C02MemoryWorking.ino
@@ -1,14 +1,8 @@
-#ifdef __arm__
-// should use uinstd.h to define sbrk but Due causes a conflict
-extern "C" char* sbrk(int incr);
-#else  // __ARM__
-extern char *__brkval;
-#endif  // __arm__
+extern int stopOptimization;
 
 int freeMemory() {
   char top;
-  __brkval = 0;  // IF COMMENTED THIS LINE, RETURN VALUE: 2299 => 0!!! (Compiler Optimization maybe.)
-  return &top - __brkval;
+  return &top - stopOptimization;
 }
 
 void setup() {
